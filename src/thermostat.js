6db2abc880420e72fbecfc7c63c044c1591e9c0f
Alex Imm <alexanderimm@hotmail.co.uk>
2017-03-16 17:54:02 +0000

    completed functionality of thermostat

--- src/thermostat.js
+++ src/thermostat.js
@@ -9,11 +9,11 @@ function Thermostat() {
   this.currentMode = true;
 }
 
-Thermostat.prototype.mode = function () {
+Thermostat.prototype.mode = function() {
   return this.currentMode;
 };
 
-Thermostat.prototype.changeMode = function () {
+Thermostat.prototype.changeMode = function() {
    this.currentMode = !this.currentMode;
    if(this.getTemp() > this.PSMAXIMUMTEMP ) {
      this.currentTemp = this.PSMAXIMUMTEMP
@@ -38,18 +38,18 @@ Thermostat.prototype.up = function () {
   this.currentTemp += 1;
 };
 
-Thermostat.prototype.down = function () {
+Thermostat.prototype.down = function() {
   if(this.getTemp() === this.MINIMUMTEMP) {
   throw new Error("Temperature is at minimum");
 }
   this.currentTemp -= 1;
 };
 
-Thermostat.prototype.reset = function () {
+Thermostat.prototype.reset = function() {
   this.currentTemp = this.DEFAULTTEMP;
 };
 
-Thermostat.prototype.checkUsage = function () {
+Thermostat.prototype.checkUsage = function() {
   var usage;
   if (this.getTemp() < 18) {
     usage = "Low-usage";
@@ -61,9 +61,27 @@ Thermostat.prototype.checkUsage = function () {
   return usage;
 };
 
-Thermostat.prototype.getPercentage = function () {
+Thermostat.prototype.getPercentage = function() {
   var percent;
   percent = this.getTemp() * 3
   percent = 100 - percent
   return percent
 };
+
+Thermostat.prototype.getPSColour = function() {
+  if (this.currentMode === true) {
+    return 'greenyellow'
+  } else {
+    return 'red'
+  }
+}
+
+Thermostat.prototype.getUsageColour = function() {
+  if (this.checkUsage() === "Low-usage") {
+    return 'greenyellow'
+  } else if (this.checkUsage() === "Medium-usage") {
+    return 'black'
+  } else if (this.checkUsage() === "High-usage") {
+    return 'red'
+  }
+}
